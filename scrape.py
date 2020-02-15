5d2d7bcd5f4a212d884ff74140a446b03ed1a2fa
GregMitch <grmitch1@gmail.com>
2020-02-15 16:43:28 -0500

    Added for loop to go through champs on webpage, and renamed variables to relevant data.

--- scrape.py
+++ scrape.py
@@ -11,11 +11,36 @@ champ = input("Champion name, spelling mistakes will screw this up >")
 champ.lower()
 url = 'https://www.op.gg/champion/'+ champ +'/statistics'
 response = requests.get(url)
-print(response) # Testing purposes, should return <Response [200]> to acknowledge a successful request
-soup = BeautifulSoup(response.text, 'html.parser')
+#print(response) #Testing purposes, should return <Response [200]> to acknowledge a successful request
+champPage = BeautifulSoup(response.text, 'html.parser')
 
 
-yeet = soup.findAll('td')[0:16:3] # gets first 'tbody' tag on page as it contains the data I require.
-print(yeet) # Testing purpose to show that tbody does indeed contain the data.
+counterData = champPage.findAll('td')[0:16:3] # ets first 'tbody' tag on page as it contains the data I require.
+#print(yeet) #Testing purposes, to show that tbody does indeed contain the data.
 
+for elem in counterData:
+    otherChamp = elem.select('img') #Following lines turns tag elements from yeet into String elements
+    if elem == counterData[0]:
+        print()
+        print('Countered by:')
+    elif elem == counterData[3]:
+        print()
+        print('Counters:')
+    otherChampString = str(otherChamp)
+    champNameStart = otherChampString.find('champion/') + 9
+    champNameEnd = otherChampString.find('.p')
+    print(otherChampString[champNameStart:champNameEnd]) 
+
+
+"""
+yaw = yeet[0].select('img') #Following lines turns tag elements from yeet into String elements
+yawString = str(yaw)
+
+# print(type(yawString)) #Confirmation that element is indeed a string.
+
+#Following lines finds the index positions for the start and end of a champions name
+champNameStart = yawString.find('champion/') + 9
+champNameEnd = yawString.find('.p')
+print(yawString[champNameStart:champNameEnd])
+"""
 
